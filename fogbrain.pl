2461b7564c241270698bd54fe9e60bac3506efea
Stefano <stefano.forti92@gmail.com>
2020-06-06 12:00:38 +0200

    Update fogbrain.pl

--- fogbrain.pl
+++ fogbrain.pl
@@ -6,16 +6,13 @@
 :-dynamic deployment/4.
 
 %%%% Thresholds to identify overloaded nodes and saturated e2e links%%%%
-bwTh(0.2). hwTh(0.5).
+hwTh(0.5).
+bwTh(0.2). 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-fogBrainT(A, NewP) :- time(fogBrain(A, NewP)).
 
 fogBrain(App, NewPlacement) :-
 	deployment(App, Placement, AllocHW, AllocBW),
-	consult('infra'),
-	time(reasoningStep(App, Placement, AllocHW, AllocBW, NewPlacement)).
+	reasoningStep(App, Placement, AllocHW, AllocBW, NewPlacement).
 fogBrain(App, Placement) :-
 	\+deployment(App,_,_,_),
-	consult('infra'),
 	time(placement(App, Placement)).
